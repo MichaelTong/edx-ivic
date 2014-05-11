85e91d9a96d07ff1ef0ebee610259e49c9282ac0
tong <tong@localhost.localdomain>
2014-05-11 14:33:53 -0500

    add files

---
+++ djangoapp/vmtemplates/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from djangoapp.vmtemplates.models import VMTemplate
+
+class VMTemplateAdmin(admin.ModelAdmin):
+	list_display = ('name', 'os_type', 'distribution', 'deploy_method', 'create_user', 'create_time')
+	search_fields = ('name','os_type', 'distribution', 'deploy_method', 'create_user__username')
+	fields = ('name','os_type','distribution','deploy_method','kernel','node_type','capabilities','memory','disk','description','repository','deploy_url')
+
+admin.site.register(VMTemplate, VMTemplateAdmin)
---
+++ djangoapp/vmtemplates/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class VMTemplate(models.Model):
+	kernel = models.CharField(max_length=30, blank=True, verbose_name='Kernel')
+        deploy_method = models.CharField(max_length=30, default='nfsmount', verbose_name='Deploy Method')
+	description = models.CharField(max_length=50, blank=True, verbose_name='Desription')
+	repository = models.CharField(max_length=100, blank=True, verbose_name='Repository')
+	node_type = models.CharField(max_length=30, default='undefined', verbose_name='Node Type')
+	deploy_url = models.CharField(max_length=200, verbose_name='Deploy URL')
+	distribution = models.CharField(max_length=30, blank=True, verbose_name='Distribution')
+	name = models.CharField(max_length=30, verbose_name='Name')
+	capabilities = models.CharField(max_length=30, default='vNode', verbose_name='Capabilities')
+        os_type = models.CharField(max_length=30, verbose_name='OS Type')
+	memory = models.IntegerField(verbose_name='Perferred Memory(MB, Only Int)')
+        disk = models.IntegerField(verbose_name='Perferred Disk Size(GB, Only Int)')
+	create_time = models.DateTimeField(verbose_name='Created Time')
+	filename = models.CharField(max_length=64, blank=True, verbose_name='Filename')
+	create_user = models.ForeignKey(User, verbose_name='Creator')
