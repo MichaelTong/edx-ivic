7f7f22dd6c1fc90d1465de94d3b286790ddd9f1d
MichaelTong <tonghao1992@gmail.com>
2014-05-12 08:45:56 -0500

    css

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -78,9 +78,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/').replace('\\','/')
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
 	os.path.join(BASE_DIR, 'templates').replace('\\','/'),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/').replace('\\','/')
+MEDIA_URL = '/media/'
--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -10,3 +11,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),
+    )
